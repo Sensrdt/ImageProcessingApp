82ed6dd24c1df9ad9c79ed69d23bd3ff0eeeac4a
Sridip Dutta <stsdutta2@gmail.com>
2020-03-07 13:10:25 +0530

    Constants.js and structuring

--- app/src/main/java/com/hyperxchange/imageprocessing/Services/UDP.java
+++ app/src/main/java/com/hyperxchange/imageprocessing/Services/UDP.java
@@ -1,14 +1,17 @@
-package com.hyperxchange.imageprocessing;
+package com.hyperxchange.imageprocessing.Services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.hyperxchange.imageprocessing.MainActivity;
+import com.hyperxchange.imageprocessing.R;
+import com.hyperxchange.imageprocessing.TakeImage;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
